115db1440b3a0860d31b6fddf525d9cb76a88788
Mac Os <badupro@Macs-MacBook-Pro.local>
2017-11-07 00:36:42 +0700

    Initial Commit

---
+++ WeatherForcast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherForcast
+//
+//  Created by badupro on 11/7/17.
+//  Copyright © 2017 badupro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherForcast/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WeatherForcast
+//
+//  Created by badupro on 11/7/17.
+//  Copyright © 2017 badupro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
